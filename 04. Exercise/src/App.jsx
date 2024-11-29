aa3a125a90aa15c6413655aa1c69795f3e8f3794
Muhammad Janisar <janisarhyder90@gmail.com>
2024-11-29 21:11:57 +0500

    exercise

---
+++ 04. Exercise/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'
+
+
+const Card = ({ title, body }) => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <p className="card-title">{title}</p>
+        <p className="card-para">{body}</p>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className='haha'>
+      {data.map(item => (
+        <Card key={item.id} title={item.title} body={item.body} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
