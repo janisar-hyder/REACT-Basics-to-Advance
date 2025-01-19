1a5d5f89bbb0429b6b15f1be14c8fb2676d43c59
Muhammad Janisar <janisarhyder90@gmail.com>
2025-01-19 21:31:38 +0500

    ok

---
+++ 05. Handling Events in React/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [btn, setbtn] = useState("Click Me")
+  const [from, setfrom] = useState({email: "", phone: ""})
+
+  const handleclick = () =>{
+    alert("button clicked");
+  };
+
+  const hover = ()=>{
+    setbtn("Click Now");
+  };
+
+  const handleChnage = (e)=>{
+    setfrom({...from, [e.target.name]:e.target.value})
+  }
+  return (
+    <>
+      <button onMouseOver={hover} onClick={handleclick}>{btn}</button>
+      <input type="text" name='email' value={from.email} onChange={handleChnage} />
+      <input type="number" name='phone' value={from.phone} onChange={handleChnage} />
+    </>
+  )
+}
+
+export default App
